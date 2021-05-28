86d8f77c62be24b0ff28a41eee35690c53c1bfc6
vtech1234 <74586697+vtech1234@users.noreply.github.com>
2021-05-28 20:07:27 +0530

    Add files via upload
    here is simple hello world program using jsp and servlet.

---
+++ HelloWorldFromServlet/src/com/first/HelloWorld.java
@@ -0,0 +1,44 @@
+package com.first;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorld
+ */
+@WebServlet(description = "A simple hello world program using Servlet.", urlPatterns = { "/HelloWorld" })
+public class HelloWorld extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public String message;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorld() {
+       message="Hello";
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Actual logic is here
+		PrintWriter out = response.getWriter();
+		out.println("<h1>"+"Hello World "+message+"</h1>");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
